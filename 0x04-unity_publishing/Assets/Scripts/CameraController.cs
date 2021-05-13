7ae95c35447a89d292c93b98d0850aa18c240dd4
Dylan Carey <dylan.carey@live.com>
2021-05-13 18:00:50 -0500

    0-2 done

---
+++ 0x04-unity_publishing/Assets/Scripts/CameraController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position + new Vector3(0f, 25.6f, -9f);
+    }
+}
---
+++ 0x04-unity_publishing/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerBody;
+    private Vector3 inputVector;
+    public float speed = 100f;
+    private int score = 0;
+    public int health = 5;
+    public Text scoreText;
+    public Text healthText;
+    public Text winloseText;
+    public Image winloseBG;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        inputVector = new Vector3(Input.GetAxisRaw("Horizontal") * speed, 0, Input.GetAxisRaw("Vertical") * speed);
+        playerBody.velocity = inputVector;
+    }
+
+    // gameplay
+    void Update()
+    {
+        SetScoreText();
+        SetHealthText();
+        if (health == 0)
+        {
+            // Debug.Log("Game Over!");
+            winloseBG.gameObject.SetActive(true);
+            winloseBG.GetComponent<Image>().color = Color.red;
+            winloseText.color = Color.white;
+            winloseText.text = "Game Over!";
+            StartCoroutine(LoadScene(3));
+        }
+
+        // Open menu with esc
+        //if (Input.GetKeyDown(KeyCode.Escape))
+        //{
+        //    SceneManager.LoadScene(0);
+        //}
+    }
+
+    // scoring
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Pickup")
+        {
+            score++;
+            Object.Destroy(other.gameObject);
+        }
+        if (other.tag == "Trap")
+        {
+            health--;
+        }
+        if (other.tag == "Goal")
+        {
+            winloseBG.gameObject.SetActive(true);
+            winloseBG.GetComponent<Image>().color = Color.green;
+            winloseText.color = Color.black;
+            winloseText.text = "You Win!";
+            StartCoroutine(LoadScene(3));
+        }
+    }
+
+    void SetScoreText()
+    {
+        scoreText.text = $"Score: {score}";
+    }
+
+    void SetHealthText()
+    {
+        healthText.text = $"Health: {health}";
+    }
+
+    IEnumerator LoadScene(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
