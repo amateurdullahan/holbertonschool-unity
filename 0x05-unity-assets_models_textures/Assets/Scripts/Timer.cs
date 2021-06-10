c1168aa6c0c0def225d1d3d7be90fdc2b7549c08
Dylan Carey <dylan.carey@live.com>
2021-06-10 15:14:29 -0500

    platformer

---
+++ 0x05-unity-assets_models_textures/Assets/Scripts/Timer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public Text TimerText;
+    public float timerValue = 0f;
+    public bool timerStart = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timerStart)
+        {
+            timerValue += Time.deltaTime;
+            float minutes = Mathf.FloorToInt(timerValue / 60);
+            float seconds = Mathf.FloorToInt(timerValue % 60);
+            float milliseconds = (timerValue % 1) * 100;
+            TimerText.text = timerValue.ToString("0:00.00");
+        }
+    }
+}
---
+++ 0x05-unity-assets_models_textures/Assets/Scripts/WinTrigger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WinTrigger : MonoBehaviour
+{
+    public Text timerText;
+    void OnTriggerEnter()
+    {
+        FindObjectOfType<Timer>().timerStart = false;
+        timerText.color = Color.green;
+        timerText.fontSize = 60;
+    }
+}
